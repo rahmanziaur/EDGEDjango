0d88e7a19c1c26f8aa87b9717d5d126fe2bc8259
Mustain Billah <34680903+mustain-billah@users.noreply.github.com>
2024-11-25 15:30:53 +0900

    Update views.py

--- App_login/views.py
+++ App_login/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 
 def register(request):
     if request.method == 'POST':
@@ -12,3 +17,9 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'App_login/register.html', {'form': form})
+
+@login_required
+def logout_user(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('homepage'))
+
